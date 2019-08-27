947f0136e1e20270776ede4904fdd5bd7d26a6a6
Graham Mc <graham20@gmail.com>
2019-08-28 07:55:59 +0900

    MongoDB work

--- node-course/task-manager/mongodb.js
+++ node-course/task-manager/mongodb.js
@@ -1,10 +1,10 @@
 // Basic CRUD operations
 
+// Destructuring
+// Equivalent to:
 // const mongodb = require('mongodb')
 // const MongoClient = mongodb.MongoClient
 // const ObjectID = mongodb.ObjectID
-
-// Destructuring
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -20,20 +20,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     // Get reference to database
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ _id: new ObjectID('5d6240ff45aa5e056af3a6d2') }, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch')
-    //     }3
-
-    //     console.log(user)
-    // })
-    
-    // db.collection('users').find({ age: 27 }).toArray( (error, users) => {
-    //     console.log(users)
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
     // })
 
-    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
-        console.log(tasks)
+    db.collection('tasks').deleteOne({
+        description: 'Task One'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
-
 })
