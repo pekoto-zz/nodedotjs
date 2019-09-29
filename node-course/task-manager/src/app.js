9c56026b8c56323975142bbfc649253e9d1b236a
Graham Mc <graham20@gmail.com>
2019-09-29 12:21:15 +0900

    unit tests

--- node-course/task-manager/src/app.js
+++ node-course/task-manager/src/app.js
@@ -4,13 +4,10 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT
 
 // Automatically parse incoming JSON
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+module.exports = app
---
+++ node-course/task-manager/src/math.js
@@ -0,0 +1,19 @@
+const calculateTip = (total, tipPercent = 0.25) => total + total * tipPercent
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be >= 0')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+module.exports = {
+    calculateTip,
+    add
+}
